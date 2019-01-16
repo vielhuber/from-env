c3d1e28d5c2cacf39ea115424cde02f4e5e6acbc
David Vielhuber <david@vielhuber.de>
2019-01-16 21:53:50 +0100

    initial push

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# envhelper
-🕶️Get environment variables from env file in npm scripts.🕶️
+# 🕶 envhelper 🕶
+envhelper is helper script that reads environment variables into command line statements.
+
+## installation
+
+```bash
+npm install envhelper
+```
+
+## usage
+
+```.env``
+```.env
+ENV1=foo
+ENV2=bar
+ENV3=baz
+ENV4=quux
+ENV5=gorge
+```
+
+```package.json``` (before)
+```json
+{
+    "scripts": {
+        "yo": "envhelper your-command --that %VARIABLE1 --needs %VARIABLE2 --some %VARIABLE3 --variables %VARIABLE1"
+    }
+}
+```
+
+```package.json``` (after)
+```json
+{
+    "scripts": {
+        "yo": "envhelper your-command --that %VARIABLE1 --needs %VARIABLE2 --some %VARIABLE3 --variables %VARIABLE1"
+    }
+}
+```
---
+++ envhelper.js
@@ -0,0 +1,25 @@
+const spawn = require('cross-spawn'),
+    exit = require('exit');
+
+// determine diff
+const oldEnv = Object.assign({}, process.env);
+require('dotenv').config();
+const newEnv = Object.assign({}, process.env);
+
+let values = {};
+Object.entries(newEnv).forEach(([newEnv__key, newEnv__value]) => {
+    if (!(newEnv__key in oldEnv)) {
+        values[newEnv__key] = newEnv__value;
+    }
+});
+
+const args = process.argv.slice(2);
+args.forEach((args__value, args__key) => {
+    if (args__value[0] === '%' && args__value.substring(1) in values) {
+        args[args__key] = values[args__value.substring(1)];
+    }
+});
+
+const command = args.shift(),
+    proc = spawn.sync(command, args, { stdio: 'inherit' });
+exit(proc.status);
